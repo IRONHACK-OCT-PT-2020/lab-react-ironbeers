c01c79cf0126278579afccc516730293d6eaab4d
Christian <christian.garba@gmail.com>
2021-03-03 17:53:10 -0700

    iteration 2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import Beers from './components/Beers';
+import RandomBeer from './components/RandomBeers';
+import NewBeer from './components/NewBeer';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Homepage />
         <Switch>
-          <Route path="/beers" />
-          <Route path="/random-beer" />
-          <Route path="/new-beer" />
+          <Route
+            exact
+            path="/"
+            render={(props) => <Homepage dog="german shephard" {...props} />}
+          />
+
+          <Route
+            exact
+            path="/beers"
+            render={(props) => <Beers dog="pit bull" {...props} />}
+          />
+          <Route
+            exact
+            path="/random-beer"
+            render={(props) => <RandomBeer dog="great dane" {...props} />}
+          />
+          <Route
+            exact
+            path="/new-beer"
+            render={(props) => <NewBeer dog="husky" {...props} />}
+          />
         </Switch>
       </div>
     );
---
+++ src/components/RandomBeers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomeButton from './HomeButton';
+
+class RandomBeers extends Component {
+  render() {
+    return (
+      <div>
+        <HomeButton />
+      </div>
+    );
+  }
+}
+
+export default RandomBeers;
