b262f021aa1b2f01eb196953ff3d44e63d310879
Ida <so.kida@hotmail.com>
2021-03-03 20:19:44 -0500

    iteration 3

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,11 +1,50 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import HomeButton from './HomeButton';
+import {Link} from 'react-router-dom'
 
 class Beers extends Component {
+state={
+  beers:[]
+}
+
+
+
+componentDidMount(){
+  axios.get('https://ih-beers-api2.herokuapp.com/beers')
+  .then(response=>{
+    console.log(response.data)
+    this.setState({
+      beers: response.data
+    })
+  })
+}
+
+
+allBeers =()=>{
+  return this.state.beers.map(eachBeer=>{
+    return(
+
+    <div key={eachBeer._id}>
+      <img src={eachBeer.image_url} alt={eachBeer.name} width='50vw'/>
+      <Link to={`/beers/${eachBeer._id}`}><h2>{eachBeer.name}</h2></Link>
+      <p>{eachBeer.tagline}</p>
+      <p>{eachBeer.contributed_by}</p>
+
+    </div>
+    )
+
+  })
+}
+
+
+
+
   render() {
     return (
       <div>
         <HomeButton />
+        {this.allBeers()}
       </div>
     );
   }
